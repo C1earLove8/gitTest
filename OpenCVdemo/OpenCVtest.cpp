d0a51f05d4c014b6184e28a9515e426d588f896b
Xiangyu Liu <1014186046@qq.com>
2018-07-04 15:07:36 +0800

    a simple test

---
+++ OpenCVdemo/OpenCVtest.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+#include<opencv2\opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat img = imread("C:\\Users\\10141\\Downloads\\testimg.jpg");
+	imshow("ÏÔÊ¾Í¼Ïñ", img);
+	waitKey(0);
+	return 0;
+}
